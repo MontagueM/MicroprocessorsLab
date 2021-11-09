70c585f2a8df065bf546d3f948760fa1bbd43c8d
Moran <msm19@ic.ac.uk>
2021-11-09 10:06:00 +0000

    Can write and read from external bus

--- main.s
+++ main.s
@@ -3,93 +3,39 @@
 psect	code, abs
 main:
 	org 0x0
-	goto	setup
+	goto	start
 	
 	org 0x100		    ; Main code starts here at address 0x100
 
-	; ******* Programme FLASH read Setup Code ****  
-setup:	
-	bcf	CFGS	; point to Flash program memory  
-	bsf	EEPGD 	; access Flash program memory
-	goto	start
-	; ******* My data and where to put it in RAM *
-myTable:
-   	db	0xf9, 0x99, 0x9f ; 0
-	db	0x66, 0x66, 0x66 ; 1
-	db	0xF8, 0x42, 0x1F ; 2
-	db	0xf8, 0xee, 0x8f ; 3
-	db	0x15, 0x5F, 0xF4 ; 4
-	db	0xf1, 0xff, 0x8f ; 5
-	db	0xf1, 0x1f, 0x9f ; 6
-	db	0xf8, 0x88, 0x88 ; 7
-	db	0xf9, 0xff, 0x9f ; 8
-	db	0xf9, 0x9f, 0x88 ; 9
-	myArray EQU 0x400	; Address in RAM for data
-	counter EQU 0x20	; Address of counter variable
-   	c1 EQU 0x10
-  	c2 EQU 0x11
- 	c3 EQU 0x12
-	align	2		; ensure alignment of subsequent instructions
-	; SET ALL PORTS TO OPEN
 
 	; ******* Main programme *********************
 start:	
     
     	movlw 	0x0
-	movwf	TRISC, A	    ; Port C all outputs
-	movlw 	0x0
-	movwf	TRISD, A	    ; Port D all outputs
-	movlw 	0x0
-	movwf	TRISE, A	    ; Port E all outputs
-    
-	lfsr	0, myArray	; Load FSR0 with address in RAM	
-	movlw	low highword(myTable)	; address of data in PM
-	movwf	TBLPTRU, A	; load upper bits to TBLPTRU
-	movlw	high(myTable)	; address of data in PM
-	movwf	TBLPTRH, A	; load high byte to TBLPTRH
-	movlw	low(myTable)	; address of data in PM
-	movwf	TBLPTRL, A	; load low byte to TBLPTRL
-	movlw	10		; 22 bytes to read
-	movwf 	counter, A	; our counter register
-	bra loop
+	; Port C is for control
+	movwf	TRISC, A
+    	movlw 	0x0
+	; Port D is for data
+	movwf	TRISD, A	
 	
-delay1:
-    	movlw 0xFF
-	movwf c2, A
-	call	delay2
-	decfsz	c1, A
-	bra	delay1
-	return
+	; Set OE* to high and CP to high, default states
+	; OE* is bit 0, CP is bit 1
+	movlw 0x1 | 0x2
+	movwf PORTC, A
 	
-delay2:
-	; 1 + 1 + 2 + 1 + 2 + 2 = 9 (loop is 
-    	movlw 0xFF
-	movwf c3, A
-	call	delay3
-	decfsz	c2, A
-	bra	delay2
-	return
+	; Set the data to be written to the chip
+	movlw 0x1 | 0x2 | 0x4 | 0x80
+	movwf PORTD, A
 	
+	; Flip CP low then high to write the data
+	movlw 0x1
+	movwf PORTC, A
 	
-delay3:
-	; 1 + 2 + 2 = 5 (loop is 1 + 2 = 3)
-	decfsz	c3, A
-	bra	delay3
-	return
-
-loop:
-	movlw 0x5A
-	movwf c1, A
-	call delay1
-        tblrd*+			; move one byte from PM to TABLAT, increment TBLPRT
-	movff	TABLAT, PORTC	
-	tblrd*+			; move one byte from PM to TABLAT, increment TBLPRT
-	movff	TABLAT, PORTD		
-	tblrd*+			; move one byte from PM to TABLAT, increment TBLPRT
-	movff	TABLAT, PORTE	
-	decfsz	counter, A	; count down to zero
-	bra	loop		; keep going until finished
+	movlw 0x1 | 0x2
+	movwf PORTC, A
 	
-	goto	0
-
+	; Make OE* low so we can see the data in LEDs
+	movlw 0x2
+	movwf PORTC, A
+	
 	end	main
