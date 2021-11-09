4cb3b2b9634d650d14b5098e8239e257c2d562d3
Moran <msm19@ic.ac.uk>
2021-11-09 11:52:24 +0000

    Serial communication works

--- main.s
+++ main.s
@@ -3,39 +3,34 @@
 psect	code, abs
 main:
 	org 0x0
+	call	SPI_MasterInit
+
 	goto	start
 	
 	org 0x100		    ; Main code starts here at address 0x100
 
-
+SPI_MasterInit:
+    bcf	    CKE2
+    movlw   (SSP2CON1_SSPEN_MASK | SSP2CON1_CKP_MASK | SSP2CON1_SSPM1_MASK)
+    movwf   SSP2CON1, A
+    bcf	    TRISD, PORTD_SDO2_POSN, A
+    bcf	    TRISD, PORTD_SCK2_POSN, A
+    return
+    
+SPI_MasterTransmit:
+    movwf   SSP2BUF, A
+Wait_Transmit:
+    btfss   SSP2IF
+    bra	    Wait_Transmit
+    bcf	    SSP2IF
+    return
+	
 	; ******* Main programme *********************
 start:	
-    
-    	movlw 	0x0
-	; Port C is for control
-	movwf	TRISC, A
-    	movlw 	0x0
-	; Port D is for data
-	movwf	TRISD, A	
-	
-	; Set OE* to high and CP to high, default states
-	; OE* is bit 0, CP is bit 1
-	movlw 0x1 | 0x2
-	movwf PORTC, A
-	
 	; Set the data to be written to the chip
-	movlw 0x1 | 0x2 | 0x4 | 0x80
-	movwf PORTD, A
-	
-	; Flip CP low then high to write the data
-	movlw 0x1
-	movwf PORTC, A
-	
-	movlw 0x1 | 0x2
-	movwf PORTC, A
-	
-	; Make OE* low so we can see the data in LEDs
-	movlw 0x2
-	movwf PORTC, A
-	
+	movlw	0x1 | 0x4 | 0x20
+	call	SPI_MasterTransmit
+	;goto 0x0
 	end	main
+	
+	
