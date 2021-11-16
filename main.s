819a7cc52c05a15ba9d873c716a3d5cdb192c7da
Moran <msm19@ic.ac.uk>
2021-11-16 09:57:46 +0000

    Working row/col inputting

--- main.s
+++ main.s
@@ -1,24 +1,9 @@
 #include <xc.inc>
 
-extrn	UART_Setup, UART_Transmit_Message  ; external subroutines
-extrn	LCD_Setup, LCD_Write_Message, LCD_Send_Byte_I
-	
-psect	udata_acs   ; reserve data space in access ram
-counter:    ds 1    ; reserve one byte for a counter variable
-delay_count:ds 1    ; reserve one byte for counter in the delay routine
-myTable__1:ds 1
-psect	udata_bank4 ; reserve data anywhere in RAM (here at 0x400)
-myArray:    ds 0x80 ; reserve 128 bytes for message data
+extrn	Keyboard_Setup
 
-psect	data    
-	; ******* myTable, data in programme memory, and its length *****
-myTable:
-	;db	'H','e','l','l','o',' ','W','o','r','l','d','!','a','b','c','d','e','f','g','h','i','j','k','!',0x0a
-	db	'T','h','e',' ','w','o','r','d',' ','o','f',' ','t','h','e',' ','d','a','y',' ','i','s',' ','a','n','c','i','e','n','t','.',0x0a
-					; message, plus carriage return
-	myTable_l   EQU	32	; length of data
-	align	2
-    
+psect	udata_acs   ; reserve data space in access ram
+
 psect	code, abs	
 rst: 	org 0x0
  	goto	setup
@@ -26,51 +11,10 @@ rst: 	org 0x0
 	; ******* Programme FLASH read Setup Code ***********************
 setup:	bcf	CFGS	; point to Flash program memory  
 	bsf	EEPGD 	; access Flash program memory
-	call	UART_Setup	; setup UART
-	call	LCD_Setup	; setup UART
+	call	Keyboard_Setup
 	goto	start
-	
-
-doTwoLoops:
-	movlw	0x10
-	lfsr	2, myArray
-	call	LCD_Write_Message
-	movlw	0x10
-	subwfb	myTable__1, 1, 1
-	movlw	11000000B	; Function set 4-bit
-	;movlw	11000000B	; Function set 4-bit
-	call	LCD_Send_Byte_I
-	tblrd*-
-	incf	myTable__1, 1
-
-	return
 
 	; ******* Main programme ****************************************
-start: 	lfsr	0, myArray	; Load FSR0 with address in RAM	
-	movlw	low highword(myTable)	; address of data in PM
-	movwf	TBLPTRU, A		; load upper bits to TBLPTRU
-	movlw	high(myTable)	; address of data in PM
-	movwf	TBLPTRH, A		; load high byte to TBLPTRH
-	movlw	low(myTable)	; address of data in PM
-	movwf	TBLPTRL, A		; load low byte to TBLPTRL
-	movlw	myTable_l	; bytes to read
-	movwf	myTable__1
-	movlw	0x11
-	cpfslt	myTable__1, 0
-	call	doTwoLoops
-	;movlw	myTable__1
-	
-	addlw	0xff		; don't send the final carriage return to LCD
-	movf	myTable__1, 0
-	call	LCD_Write_Message
-	
-	
-
-	goto	$		; goto current line in code
-
-	; a delay subroutine if you need one, times around loop in delay_count
-delay:	decfsz	delay_count, A	; decrement until zero
-	bra	delay
-	return
+start:
 
 	end	rst
